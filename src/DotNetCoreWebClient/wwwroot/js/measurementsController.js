eec683282b671da818e6183aa244f123b358df4f
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-04-24 17:15:27 +0200

    added application controller

---
+++ src/DotNetCoreWebClient/wwwroot/js/measurementsController.js
@@ -0,0 +1,12 @@
+﻿class MeasurementsController {
+    constructor() {
+        this._newMeasurementSection = new NewMeasurementSection()
+        this._newMeasurementSection.addEventListener(new class {
+            newMeasurementAdded(e) {
+                debugger
+            }
+        })
+    }
+}
+
+(() => new MeasurementsController())()
