e5da5675e10fe5a7bc05a740740ce9d77abbe4e9
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-08 17:31:28 +0200

    added service client for API including GET method

--- src/DotNetCoreWebClient/wwwroot/js/measurementsController.js
+++ src/DotNetCoreWebClient/wwwroot/js/measurementsController.js
@@ -1,5 +1,9 @@
 ﻿class MeasurementsController {
+
     constructor() {
+        
+        this._service = new MeasurementService('http://localhost:56771/')
+
         this._newMeasurementSection = new NewMeasurementSection()
         this._measurementListSection = new MeasurementListSection()
 
@@ -10,6 +14,19 @@
                 _this._measurementListSection.addNewMeasurement(e)
             }
         })
+
+        this._service.addEventListener(new class {
+            getResponseReady(e) {
+                JSON.parse(e.data).forEach(i => {
+                    _this._measurementListSection.addNewMeasurement({
+                        name: i.name,
+                        value: i.value
+                    })
+                })
+            }
+        })
+
+        this._service.get()
     }
 }
 
